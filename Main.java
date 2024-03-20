3a2309283507cb5e65c6c81854db0856f9a80df8
crrrin <crystalcraft21@gmail.com>
2024-03-20 19:58:01 -0400

    prompt for move pending also leaderboard and stuff is broken here and there

--- Main.java
+++ Main.java
@@ -8,29 +8,38 @@ Date Last Modified: March 11, 2024
 import java.io.*;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.random.*;
+import java.util.Random;
 
 
 public class Main {
     
-    public static boolean emptyUserFile = false;
+    public static boolean firstRun = true;
+    
     public static String userList = "users.txt";
     public static ArrayList<String> users = new ArrayList<String>();
     public static ArrayList<Integer> scores = new ArrayList<Integer>();
+    public static boolean emptyUserFile = false;
     public static boolean nameAlreadyExists = false;
+    
+    public static int numCurrentUser = 1;
+    public static String user1;
+    public static String user2;
+    public static boolean resetUsers = true;
+    public static int move;
+    public static int moveCount = 0;
+    
+    public static String xUser;
+    public static String oUser;
+    public static String winner;
+
     public static String userMode = "userMode";
     public static String menuMode = "menuMode";
     public static String moveMode = "moveMode";
-    public static boolean playAgain = true;
-    public static boolean winyet = false;
-    public static int numCurrentUser = 1;
-
-    public static String user1;
-    public static String user2;
-
-    public static String xUser;
-    public static String oUser;
     
+    public static boolean winyet = false;
+    
+    public static boolean playAgain = true;
+
     public static String gameBoard[][] = {
         {"1", "2", "3"},
         {"4", "5", "6"},
@@ -61,7 +70,7 @@ public class Main {
             numCurrentUser = 1;
             game(s);
         } else if (choice == 2) {
-            instructions();
+            instructions(s);
         } else {
             leaderboard();
         }
@@ -69,36 +78,109 @@ public class Main {
     }
 
     public static void leaderboard() {
-        // TODO Auto-generated method stub
+        // TODO leaderboard is pending
         throw new UnsupportedOperationException("Unimplemented method 'leaderboard'");
     }
 
     public static void game(Scanner s) {
-        // TODO Auto-generated method stub
+        // TODO main game loop is pending
 
         while (playAgain) {
-            int moves = 0;
+
+            numCurrentUser = 1;
             
-            user1 = inputCheck(s, userMode);
+            if (resetUsers){
+                user1 = inputCheck(s, userMode);
 
-            numCurrentUser += 1;
+                numCurrentUser += 1;
 
-            user2 = inputCheck(s, userMode);
+                user2 = inputCheck(s, userMode);
+            }
 
             printBoard();
 
-            inputCheck(s, moveMode);
+            randomizeFirstPlayer();
+            
+            while (moveCount < 9 && !winyet){
+                if ((moveCount % 2) ==)
+                System.out.print("It's " + xUser + "'s (X) turn! Choose an available space from 1-9: ");
+                // TODO ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3 :3
+
+                move = Integer.parseInt(inputCheck(s, moveMode));
+
+                if (moveCount%2 == 1) {
+                    gameBoard[(move-1)/3][(move-1)%3] = "X";
+                } else {
+                    gameBoard[(move-1)/3][(move-1)%3] = "O";
+                }
+                
+                if (moveCount > 3) {checkWin();}
+
+                moveCount++;
+
+            }
+
+            if (!winyet) {
+                winner = "tie";
+            }
+
+            if (winner == "X"){
+                System.out.println(xUser + "(X) Wins!");
+                scoreIncrement(xUser);
+            } else if (winner == "O") {
+                System.out.println(oUser + "(O) Wins!");
+                scoreIncrement(oUser);
+            } else {
+                System.out.println("Tie game!");
+            }
+
+            sortFile();
 
 
 
-            // while (moves < 9 && !winyet){
+        }
+    }
 
-            //     for (int i = )
-
-
-            // }
+    public static void checkWin(){
+        
+        // diagonal win checking
+        for (int diagonal = 0; diagonal < 3; diagonal++){
+            if (gameBoard[diagonal][diagonal] == gameBoard[diagonal][diagonal] && gameBoard[diagonal][diagonal] == gameBoard[diagonal][diagonal]){
+                winyet = true;
+                winner = gameBoard[diagonal][diagonal];
+                return;
+            }
+        }
+        
+        // column win checking
+        for (int row = 0; row < 3; row++){
+            if (gameBoard[0][row] == gameBoard[1][row] && gameBoard[1][row] == gameBoard[2][row]){
+                winyet = true;
+                winner = gameBoard[0][row];
+                return;
+            }
         }
 
+        // row win checking
+        for (int column = 0; column < 3; column++){
+            if (gameBoard[column][0] == gameBoard[column][1] && gameBoard[column][1] == gameBoard[column][2]){
+                winyet = true;
+                winner = gameBoard[column][0];
+                return;
+            }
+        }       
+    }
+
+    public static void randomizeFirstPlayer() {
+        Random rand = new Random();
+
+        if (rand.nextBoolean()) {
+            xUser = user1;
+            oUser = user2;
+        } else {
+            xUser = user2;
+            oUser = user1;
+        }
 
     }
 
@@ -118,13 +200,17 @@ public class Main {
             "+-~-+-~-+-~-+");
     }
 
-    public static void instructions() {
+    public static void instructions(Scanner s) {
         clearConsole();
         System.out.println("Compete against a friend (or yourself) in order to get three X's or O's in a row!");
         System.out.println("Whoever is first to get three of their symbol, be it an X or an O, in a row, column, or diagonal wins!");
         System.out.println("When prompted to move, select the number on the grid, from 1-9, at the location that you wish to play.");
         System.out.println("If said move is possible, the move will be completed.");
         System.out.println("If said move is NOT possible, you will be prompted to make a valid move.");
+
+        System.out.println("Hit enter to return to the main menu.");
+
+        s.nextLine();
 
     }
 
@@ -139,8 +225,11 @@ public class Main {
         // for if the function is called to get player names...
         if (mode.equals(userMode)) {
             
-            System.out.print("Choose a username for Player " + numCurrentUser + ": ");
-            inputtedValue = s.nextLine();
+            
+            while ((inputtedValue.strip()).equals("")) {
+                System.out.print("Choose a username for Player " + numCurrentUser + ": ");
+                inputtedValue = s.nextLine();
+            }
 
             while ((numCurrentUser == 2) && inputtedValue.equals(user1)) {
 
@@ -151,6 +240,7 @@ public class Main {
 
             String userName = "";
             int wins = 0;
+            nameAlreadyExists = false;
 
             try {
                 fileScanner = new Scanner(new BufferedReader(new FileReader(userList)));
@@ -159,28 +249,26 @@ public class Main {
                 emptyUserFile = !(fileScanner.hasNext());
 
                 
-                while ((fileScanner.hasNext()) && numCurrentUser == 1) {
+                while ((fileScanner.hasNext()) && firstRun) {
                     wins = fileScanner.nextInt();
-                    
-                    while (userName.equals("")) {
-                        userName = (fileScanner.nextLine()).strip();
-                 
-                    }
-                    if (!nameAlreadyExists){
-                    nameAlreadyExists = (inputtedValue.equals(userName));}
+                       
+                    userName = (fileScanner.nextLine()).strip();
                     
                     users.add(userName);
                     scores.add(wins);
+                    
                     // userScores.add(wins + " " + name);
                     
                 } 
 
+                nameAlreadyExists = users.contains(inputtedValue);
+                
                 if (!nameAlreadyExists){
                     
-                    if (!nameAlreadyExists) {
+                    // if (!nameAlreadyExists) {
                         users.add(inputtedValue);
                         scores.add(0);
-                    }
+                    // }
 
                     if (emptyUserFile) {
                         fileWriter.print("0 " + inputtedValue);
@@ -191,6 +279,8 @@ public class Main {
 
             } catch (Exception e) {
             } finally {
+                firstRun = false;
+
                 if (fileScanner != null){
                     fileScanner.close();
                 }
@@ -199,6 +289,7 @@ public class Main {
                     
                     sortFile();
                 }
+                
             }
             
             return inputtedValue;
@@ -217,7 +308,7 @@ public class Main {
                     if (!(choice >= 1 && choice <= 3)) throw new Exception("💩");
                 } catch (Exception e) {
                     choice = -1;
-                    s.nextLine();
+                    // s.nextLine();
                     clearConsole();
                     System.out.println("★ Main Menu ★\n1. Play\n2. How To Play\n3. Leaderboard");
 
@@ -236,23 +327,22 @@ public class Main {
 
             {
                 try {
-                System.out.print("Select an available grid space from 1-9: ");
-                choice = s.nextInt();
-                if (!(choice >= 1 && choice <= 9)
-                    || gameBoard[(choice-1)/3][(choice-1)%3] == "X"
-                    || gameBoard[(choice-1)/3][(choice-1)%3] == "O") 
-                        throw new Exception("💩");
-                        // TODO make this exception descriptive
-            
-                    return Integer.toString(choice);
+                    choice = s.nextInt();
+                    if (!(choice >= 1 && choice <= 9)
+                        || gameBoard[(choice-1)/3][(choice-1)%3] == "X"
+                        || gameBoard[(choice-1)/3][(choice-1)%3] == "O") 
+                            throw new Exception("💩");
+                            // TODO make this exception descriptive
+                
+                        return Integer.toString(choice);
 
-                } catch (Exception e) {
-                    choice = -1;
-                    s.nextLine();
-                    clearConsole();
-                    printBoard();
+                    } catch (Exception e) {
+                        choice = -1;
+                        s.nextLine();
+                        clearConsole();
+                        printBoard();
 
-                }
+                    }
             }
         }
         
